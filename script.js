1190eefa1c0b7f1fa5bae79f1680cdcb2c926c41
Arman Urstem <armanurstem@gmail.com>
2024-11-12 16:30:06 +0500

    Initial commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = '7045741a000ae137ad8ebd7bca2a4847';
+const API_KEY = 'YOUR_TMDB_API_KEY';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 
